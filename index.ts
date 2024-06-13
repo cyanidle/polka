bf02713ee57c88aa801e1209acf2d6b7ad70b7c2
Alexej Doronin <lyosha.doronin@gmail.com>
2024-06-13 23:02:48 +0200

    damn

--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+import van from "mini-van-plate/van-plate"
+
+const {h2, div,} = van.tags;
+
+const main = (req: Request) => div(
+    h2("HELLO!")
+)
+
+const doc = (req: Request) => `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Polka</title>
+    <meta charset="utf-8">
+    <script>
+    
+    </script>
+</head>
+<body>
+    ${main(req).render()}
+</body>
+</html>
+`
+
+const server = Bun.serve({
+    development: true,
+    hostname: "0.0.0.0",
+    port: 3000,
+    fetch: async (req: Request) => {
+        const res = new Response(doc(req));
+        res.headers.append('Content-Type', 'text/html; charset=utf-8');
+        return res;
+    }
+})
+
+console.log(`Serving on http://${server.hostname}:${server.port} ...`)
